b085b7afc46b757f665d840795f2f2798a56115d
Antonino1991 <antoninoscarfone91@gmail.com>
2019-09-16 16:16:49 +0200

    appunto lezione1

--- src/s024/Simple.java
+++ src/s024/Simple.java
@@ -1,7 +1,9 @@
 /*
  * A simple Java source file
+ * un comando finisce con un punto e virgola rappresenta la fine dello statement
+ * le parentesi graffe indicano un blocco di calcolo, le tonde indicano un metodo, le parentesi quadre indicano un array
  */
-package s024;
+package s024; 
 
 import java.lang.Math; // not required
 
