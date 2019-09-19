00be38da4e3533f5f20797eef85b73da194e8a43
Antonino1991 <antoninoscarfone91@gmail.com>
2019-09-19 15:33:38 +0200

    1esercizio

---
+++ src/s005/SpeedX.java
@@ -0,0 +1,14 @@
+package s005;
+
+public class SpeedX {
+
+	public static double speed(double distance, double time) {
+		double speed = 0;
+		if (time == 0) {
+			return 0;
+		} else
+			speed = distance / time;
+		return 0;
+	}
+
+}
