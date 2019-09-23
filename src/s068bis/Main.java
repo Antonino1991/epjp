96e97661853d639e68bb8bddc85e02b44f10ac86
Administrator <Administrator@192.168.20.63>
2019-09-23 17:49:21 +0200

    23.9.19

--- src/s068bis/Main.java
+++ src/s068bis/Main.java
@@ -8,7 +8,15 @@ public class Main {
 		persone[1] = new Dipendente("Luca", "Bianchi", 1900);
 		persone[2] = new Consulente("Marco", "Belli", 700);
 		persone[3] = new Dipendente("Anna", "Rossi", 500);
-//for (Persona : )
-	}
+		for (Persona person : persone) {
+			if (person instanceof Consulente) {
+				Consulente c = (Consulente) person;
+				System.out.println(c.getFattura());
+			} else if (person instanceof Dipendente) {
+				Dipendente e = (Dipendente) person;
+				System.out.println(e.getSalario());
+			}
+		}
 
+	}
 }
--- src/s077/Simple.java
+++ src/s077/Simple.java
@@ -7,4 +7,34 @@ public class Simple {
 		}
 		return -value;
 	}
+
+	public int negate2(int value) {
+		if (value == Integer.MIN_VALUE) {
+			throw new IllegalArgumentException("Can't negate MIN_VALUE");
+		}
+		return -value;
+	}
 }
+
+//	public static void main(String[] args) {
+//		Simple simple = new Simple();
+//		try {
+//		int x = simple.negate2(Integer.MIN_VALUE);
+//		System.out.println("my value negated is :" + x);
+//	} catch (MyBegateException  ) {
+//		System.out.println();
+//	}
+//}
+//
+//class MyNegateException extends Exception {
+//	private int alternativeValue;
+//
+//	public MyNegateException(int alternativevalue) {
+//		this.alternativeValue = alternativeValue;
+//
+//	}
+//
+//	public int getAlternativeValue() {
+//		return alternativeValue;
+//	}
+//}
